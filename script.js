38822fbe1a25f87d1150e28f7d12be307b07b002
rohitt0007 <rohittr0070@gmail.com>
2024-09-11 23:46:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// 1 word vs keyword ->
+// rohit -> word hai isko ham hmesa string me likhenge chahe 
+// vo ank hi ku na ho jaise 12 
+// exaple--> "rohit" "12"
+
+// 2 keyword -> var , loop, forEach
+
+// priting method / function 
+//keyword()-> method/function call
+//word()
+// console.log("hello world", "kya hal hai");
+// console.error("hello world")
+// console.warn("hello world")
+
+//variables
+//var varname = "value/data/information"
+
+//jaise yha -> id -> word hai 
+
+
+// var id = "ihihiyilouoiuturyr"
+
+//id -> yha hmne iske ange ->var<- 
+//lga kr id ko keyword bna dia
+
+// console.log(id);
+
+//. naming conversion of the variables
+//1 must start with small/capital/_nad must not staart with number.
+//2 variable name must not include any special chaerctor other than_
+// 3 variable name must a singke in a single word
+//4 young_age -> snack_case_pattern
+//teenAgeOther -> camelCasePattern
+//OldAgeOther -> PascalCasePattern
+//kabab-case-pattern XXXXXXXXXXX
+//5 variable name must be relevent to its's value and unique 
+//var age = "sheryians coding school"
+
+// 1. number -> NaN , infinity , -infinity
+// var age = 12;
+// var age = 12 + 12;
+// console.log(age);
+// console.log(typeof age)
+
+// 2. sting -> '' , "" , ` ` ;
+//var address = "bhopal";
+// concatination -> jodna ya add krna string ko jodna 
+// var address = "city" + "bhopa;"
+//console.log(address);
+//console.log(typeof address)
+
+//single line string = '' / ""
+// multiple line string -> ` ` -> ise ham (template literals) kahte hai
+
+// var details = `Name
+//age
+//`Address
+//console.log(details);
+//console.log(tyeof details);
+
+
+
+// var username = "rohit";
+// var age = 20;
+// console.log("Username is", username, "and the Age is", age);
+// console.log(`Username is ${username}and the Age is ${age}`);
+
+// 3.boolean -> false, true;
+// var isValid = fasle;
+// console.log(isValid);
+// console.log(typeof isValid);
+
+//4 null
+
+// var age =  23
+// console.log(age);
+// console.log(typeof age);
+// age = null;
+// console.log(age)
+// console.log(typeof age);
+
+//5 undefind
+
+// var a ; //declearation
+// console.log(a)
+// console.log(typeof a)
+
+
+// a = 12; // initialization
+// console.log(a);
+
+
+// a = 45; // reinitialzation/updation
+// console.log(a);
